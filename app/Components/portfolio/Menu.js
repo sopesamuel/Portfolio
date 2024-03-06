9ea41737e0723e457b62d7ab277949a8c3e8bca6
sopesamuel <fasusimosope@gmail.com>
2024-03-06 14:51:25 +0100

    Another Portfolio

--- app/Components/portfolio/Menu.js
+++ app/Components/portfolio/Menu.js
@@ -2,27 +2,34 @@
 const Menu =[
   {
     id:1,
+    Image: "https://res.cloudinary.com/dmo4e3lgg/image/upload/v1709732904/Screenshot_2024-03-06_at_14.43.37_ctj1kn.jpg",
+    title:"ZenfiPay",
+    category: "Frontend",
+    demo: "https://waitlist.zenfipay.com/"
+  },
+  {
+    id:2,
     Image: "https://res.cloudinary.com/dmo4e3lgg/image/upload/v1709410594/Screenshot_2024-03-02_at_20.59.41_r2o11r.jpg",
     title:"Foosion NG",
     category: "Frontend",
     demo: "https://foosion.ng/"
   },
   {
-    id:2,
+    id:3,
     Image: "https://res.cloudinary.com/dmo4e3lgg/image/upload/v1709410592/Screenshot_2024-03-02_at_20.59.17_z7i2zh.jpg",
     title:"Wiping",
     category: "Frontend",
     demo: "https://www.wiping.tech/"
   },
   {
-    id:3,
+    id:4,
     Image: "https://res.cloudinary.com/dmo4e3lgg/image/upload/v1709410591/Screenshot_2024-03-02_at_20.57.25_rjegmn.jpg",
     title:"Ecommerce CheckOut Page",
     category: "Frontend",
     demo: "https://sopesamuel-ecommerce-product-page.netlify.app/"
   },
   {
-    id:4,
+    id:5,
     Image: "https://res.cloudinary.com/dmo4e3lgg/image/upload/v1709411704/Cover_design_ktozxd.png",
     title:"Spotify UI APP",
     category: "Applications",
@@ -30,7 +37,7 @@ const Menu =[
   }
   ,
   {
-    id:5,
+    id:6,
     Image: "https://res.cloudinary.com/dmo4e3lgg/image/upload/v1709412085/Screenshot_2024-03-02_at_21.39.18_k2rq8p.jpg",
     title:"Behance Profile",
     category: "UI Design",
